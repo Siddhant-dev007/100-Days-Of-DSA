c42fad98c642351604c1cbc50f0c1f5335c2c0a2
Siddhant-dev007 <siddhant2007s@gmail.com>
2026-04-07 12:00:00 +0530

    Day 63: DSA Solution

---
+++ Day_63.c
@@ -0,0 +1,70 @@
+/*Problem Statement:
+Perform Depth First Search (DFS) traversal of a graph starting from vertex 0.
+
+Input Format:
+- First line contains V (vertices) and E (edges)
+- Next E lines each contain two integers u and v (edge between u and v)*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+struct Node {
+    int dest;
+    struct Node* next;
+};
+
+struct Node* adjList[MAX];
+int visited[MAX];
+
+struct Node* newNode(int dest) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->dest = dest;
+    node->next = NULL;
+    return node;
+}
+
+void addEdge(int u, int v) {
+    struct Node* node = newNode(v);
+    node->next = adjList[u];
+    adjList[u] = node;
+
+    node = newNode(u);
+    node->next = adjList[v];
+    adjList[v] = node;
+}
+
+void dfs(int v) {
+    visited[v] = 1;
+    printf("%d ", v);
+
+    struct Node* curr = adjList[v];
+    while (curr) {
+        if (!visited[curr->dest])
+            dfs(curr->dest);
+        curr = curr->next;
+    }
+}
+
+int main() {
+    int v, e;
+    scanf("%d %d", &v, &e);
+
+    for (int i = 0; i < v; i++) {
+        adjList[i] = NULL;
+        visited[i] = 0;
+    }
+
+    for (int i = 0; i < e; i++) {
+        int u, w;
+        scanf("%d %d", &u, &w);
+        addEdge(u, w);
+    }
+
+    printf("DFS Traversal: ");
+    dfs(0);
+    printf("\n");
+
+    return 0;
+}
